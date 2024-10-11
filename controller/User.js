a47d95c30fefc0a3abab5e240be978cdb26d67c8
manishgripp <manish.kumar@gripinvest.in>
2024-10-11 15:49:48 +0530

    removed duplicate code

--- controller/User.js
+++ controller/User.js
@@ -54,45 +54,21 @@ router.post('/resolve-query',(req,res)=>{
 })
 
 
-router.post('/issue-categorizer',(req,res)=>{
-    const {question,description,email_id} =  req.body
-    const maskedQuery = regex.replaceSensativeInformation(question)
-    const maskedDescription =regex.replaceSensativeInformation(description)
-    const data = {
-        question: maskedQuery,
-        description: maskedDescription,
-        email_id
-      };
-    axios.post(`${process.env.RetoolIssueCategorizerUrl}startTrigger`,data,{
-        headers:{
-            'Content-Type': 'application/json'
-        },
-        params:{
-            'workflowApiKey':process.env.RetoolIssueCategorizerAuth
-        }
-    }).then((response)=>{
-        res.status(200).json(response.data)
-    })
-    .catch((err)=>{
-        console.log(err);
-        res.status(400).json({"msg":"Error in retool AI workflow"})
-    })
-})
-
 router.post('/get-pii-data',(req,res)=>{
     const {email_id} =  req.body
     const data = {
         email_id
       };
     
-      axios.post(`${process.env.RetoolCSPIIDataWorkflowURL}startTrigger`,data,{
+      axios.post(`${process.env.RetoolCSPIIDataWorkflowURL}/startTrigger`,data,{
         headers:{
             'Content-Type': 'application/json'
         },
         params:{
             'workflowApiKey':process.env.RetoolCSPIIDataWorkflowAuth
         }
-    }).then((response)=>{
+    })
+    .then((response)=>{
         res.status(200).json(response.data)
     })
     .catch((err)=>{
